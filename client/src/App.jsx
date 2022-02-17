aa65a0fe54800770e542a3fbd6ac801014b840ec
ZnoGouDj <znogoud3@gmail.com>
2022-02-18 00:03:02 +0300

    Add basic structure, styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Navbar, Welcome, Footer, Services, Transactions } from './components';
 import './index.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   );
 };
